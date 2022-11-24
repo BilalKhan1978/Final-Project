4c5bc876fc9a2eacdc6f9eb2e4f7ec9893814a21
Bilal Umar <bilalumar28@hotmail.com>
2022-11-24 10:58:12 +0100

    Final Project

---
+++ JustGoApi/JustGoApi/Models/Listing.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.InMemory.ValueGeneration.Internal;
+
+namespace JustGoApi.Models
+{
+    public class Listing
+    {
+      public int Id { get; set; }
+      public string Title { get; set; }
+      public byte[] Image { get; set; }
+      public float Price { get; set; }
+      public Category CategoryId { get; set; }
+      public User UserId { get; set; }
+    }
+}
